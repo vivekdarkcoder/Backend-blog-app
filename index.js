758ea529f40d32ddd3e5e24513083884388680e1
vk3796565@gmail.com <vk3796565@gmail.com>
2022-04-05 11:03:03 +0530

    changes

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL,{
+useNewUrlparser: true,
+useUnifieldTopology: true,
+
+})
+.then(console.log("connected to MongoDB"))
+.catch((err)=>console.log(err));
+
 
 app.use("/" , (req,res)=>{
     console.log("hey this is main url");
