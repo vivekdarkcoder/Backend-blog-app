39e40104c7006e6e95e43ba70efde9541f4299f5
vk3796565@gmail.com <vk3796565@gmail.com>
2022-04-08 10:10:12 +0530

    changes

--- index.js
+++ index.js
@@ -2,20 +2,27 @@ const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+// const DB = 'mongodb+srv://vivek_kumar:La8VWbv9au2YvEP@cluster0.5mxfu.mongodb.net/blogdata?retryWrites=true&w=majority';
 
-dotenv.config();
-mongoose.connect(process.env.MONGO_URL,{
-useNewUrlparser: true,
-useUnifieldTopology: true,
+// mongoose.connect(DB,{
+//     useNewUrlParser: true,
+   
+// }).then(()=>{ 
+//     console.log("connection done");
+// }).catch((err)=> console.log(err));
 
+dotenv.config({path:'./.env'});
+const DB = process.env.MONGO_URL;
+app.use(express.json())
+mongoose.connect(DB,{
+    useNewUrlParser: true,
 })
 .then(console.log("connected to MongoDB"))
 .catch((err)=>console.log(err));
 
+app.use("/server/auth", authRoute)
 
-app.use("/" , (req,res)=>{
-    console.log("hey this is main url");
-})
 app.listen("5000", ()=>{
     console.log("backend is running");
 })
